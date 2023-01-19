05f757696c59a13a15e87b1120b9989251d494d3
StyxSlayer <83481046+StyxSlayer@users.noreply.github.com>
2023-01-18 23:30:31 -0800

    grid + added circle anim

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ addEventListener('scroll', (event) => {
 });
 
 var infoStatements = [
-    "We're the strategy department of team 3128. Starting in 2020, we've tried to develop a series of applications to digitize our scouting process.",
+    "We're the strategy department of team 3128. Starting in 2020, we've developed a series of applications to digitize our scouting process.",
     "Scouting is basically glorified stalking. We take notes of what other robots can do on the field, and we try to strategize with and against them.",
     "Ugh so many questions. Keep on scrolling to see our documentation, methodology, and github. Jokes aside, thanks for visiting :)"    
 ]
@@ -73,3 +73,72 @@ window.addEventListener('scroll', function() {
     }
 });
 
+function init(){
+	new SmoothScroll(document,120,30)
+}
+
+function SmoothScroll(target, speed, smooth) {
+	if (target === document)
+		target = (document.scrollingElement 
+              || document.documentElement 
+              || document.body.parentNode 
+              || document.body) // cross browser support for document scrolling
+      
+	var moving = false
+	var pos = target.scrollTop
+  var frame = target === document.body 
+              && document.documentElement 
+              ? document.documentElement 
+              : target // safari is the new IE
+  
+	target.addEventListener('mousewheel', scrolled, { passive: false })
+	target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
+
+	function scrolled(e) {
+		e.preventDefault(); // disable default scrolling
+
+		var delta = normalizeWheelDelta(e)
+
+		pos += -delta * speed
+		pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
+
+		if (!moving) update()
+	}
+
+	function normalizeWheelDelta(e){
+		if(e.detail){
+			if(e.wheelDelta)
+				return e.wheelDelta/e.detail/40 * (e.detail>0 ? 1 : -1) // Opera
+			else
+				return -e.detail/3 // Firefox
+		}else
+			return e.wheelDelta/120 // IE,Safari,Chrome
+	}
+
+	function update() {
+		moving = true
+    
+		var delta = (pos - target.scrollTop) / smooth
+    
+		target.scrollTop += delta
+    
+		if (Math.abs(delta) > 0.5)
+			requestFrame(update)
+		else
+			moving = false
+	}
+
+	var requestFrame = function() { // requestAnimationFrame cross browser
+		return (
+			window.requestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.oRequestAnimationFrame ||
+			window.msRequestAnimationFrame ||
+			function(func) {
+				window.setTimeout(func, 1000 / 50);
+			}
+		);
+	}()
+}
+init()
